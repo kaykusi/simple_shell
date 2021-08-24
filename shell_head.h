33ab9f5f9c1debe480a018dffd0cc307df6fbb32
Akwesi Kusi <therealkusi@gmail.com>
2021-08-24 13:22:21 -0700

    beautiful code

---
+++ shell_head.h
@@ -0,0 +1,13 @@
+#ifndef _SHELL_HEAD_H__
+#define _SHELL_HEAD_H_
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/* Shell Libraries */
+int print_number(int);
+int _putchar(char);
+
+#endif /* _SHELL_HEAD_H_*/
